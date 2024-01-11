a13cbf4a2b96678fbecee896d6e19abd9a8d643b
jiangyi <jiangyi@corp-ci.com>
2024-01-11 17:35:15 +0800

    移除不必要的"

--- src/BaseUI/Welcome/LogoIcon.tsx
+++
@@ -1,7 +0,0 @@
-import LogoSvg from "../../assets/menu_logo.svg";
-import React from "react";
-
-export const MenuLogo: React.FC<any> = (props: any) => {
-	const { className } = props;
-	return <img src={LogoSvg} className={className} />;
-};

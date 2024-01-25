0ec61a2cd31040666e4dfbce74ae79b7228f2bd6
jiangyi <jiangyi@corp-ci.com>
2024-01-25 13:52:25 +0800

    fix dayjs

--- src/components/Dashboard/FormModal/TypeEditor/TypeDateTime.tsx
+++ src/components/Dashboard/FormModal/TypeEditor/TypeDateTime.tsx
@@ -6,8 +6,19 @@ import React from "react";
 import { DatePicker, DatePickerProps } from "antd";
 import { TableColumnItem } from "../../../../store/workflowSlice";
 import dayjs from "dayjs";
-// import customParseFormat from 'dayjs/plugin/customParseFormat';
-// dayjs.extend(customParseFormat);
+import advancedFormat from "dayjs/plugin/advancedFormat";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import localeData from "dayjs/plugin/localeData";
+import weekday from "dayjs/plugin/weekday";
+import weekOfYear from "dayjs/plugin/weekOfYear";
+import weekYear from "dayjs/plugin/weekYear";
+
+dayjs.extend(customParseFormat);
+dayjs.extend(advancedFormat);
+dayjs.extend(weekday);
+dayjs.extend(localeData);
+dayjs.extend(weekOfYear);
+dayjs.extend(weekYear);
 
 interface TypeDateTimeProps {
 	mode?: "multiple";
@@ -16,7 +27,9 @@ interface TypeDateTimeProps {
 	setForm: any;
 }
 
-const TypeDateTime: React.FC<TypeDateTimeProps> = (props: TypeDateTimeProps) => {
+const TypeDateTime: React.FC<TypeDateTimeProps> = (
+	props: TypeDateTimeProps,
+) => {
 	const { cell, form, setForm } = props;
 
 	const value = form[cell.key] || null;
@@ -25,11 +38,19 @@ const TypeDateTime: React.FC<TypeDateTimeProps> = (props: TypeDateTimeProps) => 
 		console.log(date, dateString);
 		setForm({
 			...form,
-			[cell.key]: dateString
+			[cell.key]: dateString,
 		});
 	};
 
-	return <DatePicker value={value && dayjs(value)} showTime={{ format: "HH:mm" }} format="YYYY-MM-DD HH:mm" onChange={onChange} style={{ width: "100%" }} />;
+	return (
+		<DatePicker
+			value={value && dayjs(value)}
+			showTime={{ format: "HH:mm" }}
+			format="YYYY-MM-DD HH:mm"
+			onChange={onChange}
+			style={{ width: "100%" }}
+		/>
+	);
 };
 
 export default TypeDateTime;

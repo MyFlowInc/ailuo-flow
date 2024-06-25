d1f942b5f7c528f0b2043285833670b124820e29
originalchou <c.zhou.ca@gmail.com>
2024-06-25 16:56:33 +0800

    update incoming status view

--- src/pages/PM/WorkshopStatus/Incoming.tsx
+++ src/pages/PM/WorkshopStatus/Incoming.tsx
@@ -1,4 +1,4 @@
-import { Button, ConfigProvider, Pagination, Table, Tag } from "antd";
+import { Button, ConfigProvider, Pagination, Table, Tag, message } from "antd";
 import React, { useRef, useState } from "react";
 import { LeftOutlined } from "@ant-design/icons";
 import { useHistory, useParams } from "react-router";
@@ -7,56 +7,103 @@ import TableColumnRender from "../../../components/Dashboard/TableColumnRender";
 import PurchaseTable from "./IncomingPurchase/PurchaseTable";
 import EditFilled from "../../../assets/icons/EditFilled";
 import { getStore } from "../../../store";
+import {
+	selectIsManager,
+	selectIsWorkshop,
+	setCurWorkshop,
+} from "../../../store/globalSlice";
+import { useAppDispatch, useAppSelector } from "../../../store/hooks";
+import {
+	getNextActionsByTypeAndStatus,
+	getTagColorByStatus,
+	getLabel,
+	updateStatusByStage,
+} from "../WorkshopManage";
+import { Status, Stage } from "../types";
+import { useSelector } from "react-redux";
+import { getWorkshopManagement } from "../../../api/ailuo/workshop";
 
-const StatusView = () => {
-	const curWorkshop = { ...getStore("global.curWorkshop") };
-	const params = useParams<any>()
+const StatusView = (props: {
+	id: string;
+	status: Status;
+	fecthWorkshop: () => void;
+	relatedProjectId: string;
+}) => {
+	const params = useParams<any>();
 	const history = useHistory();
+	const stage: Stage = "incoming";
+	const isManager = useAppSelector(selectIsManager);
+	const isWorkshop = useAppSelector(selectIsWorkshop);
+	const action = getNextActionsByTypeAndStatus(
+		stage,
+		props.status,
+	)?.[0] as Status;
 	return (
 		<div className="flex items-center mt-2">
 			<div className="flex items-center">
 				<div className="mr-2 text-[#848484]">状态: </div>
 				<Tag
-					className="flex items-center"
-					color={"#E8F2FF"}
-					style={{ color: "#000", height: "27px" }}
+					color={getTagColorByStatus(props.status)}
+					style={{ color: "#000" }}
 				>
-					{"未启动"}
+					{getLabel(stage, props.status, "statusLabel")}
 				</Tag>
 			</div>
-			<div className="flex  items-center ml-4">
-				<div className="mr-2 text-[#848484]">操作: </div>
-				<Tag
-					className="cursor-pointer flex items-center"
-					color={"#D4F3F2"}
-					style={{ color: "#000", height: "27px" }}
-				>
-					{"开始备料"}
-				</Tag>
-				<ConfigProvider theme={blueButtonTheme}>
-					<Button
-						className="ml-4"
-						type="primary"
-						icon={<EditFilled></EditFilled>}
-						onClick={() =>
-							history.push(
-								`/dashboard/work-shop-manage/${params.wspId}/incoming/purchase/new`,
-							)
-						}
+			{action && (
+				<div className="flex  items-center ml-4">
+					<div className="mr-2 text-[#848484]">操作: </div>
+					<Tag
+						className="cursor-pointer"
+						color={"#D4F3F2"}
+						style={{ color: "#000" }}
+						onClick={() => {
+							updateStatusByStage(
+								props.id,
+								stage,
+								action,
+								isManager || isWorkshop,
+								props.fecthWorkshop,
+								props.relatedProjectId,
+							);
+						}}
 					>
-						新建请购单
-					</Button>
-				</ConfigProvider>
-			</div>
+						{getLabel(stage, action, "actionLabel")}
+					</Tag>
+				</div>
+			)}
+			<ConfigProvider theme={blueButtonTheme}>
+				<Button
+					className="ml-4"
+					type="primary"
+					icon={<EditFilled></EditFilled>}
+					onClick={() =>
+						history.push(
+							`/dashboard/work-shop-manage/${params.wspId}/incoming/purchase/new`,
+						)
+					}
+				>
+					新建请购单
+				</Button>
+			</ConfigProvider>
 		</div>
 	);
 };
-
 interface IncomingProps {}
 
 const Incoming: React.FC<IncomingProps> = () => {
 	const history = useHistory();
+	const workshop = useSelector((state) => (state as any).global.curWorkshop);
+	const dispatch = useAppDispatch();
+	const params = useParams<{ wspId: string }>();
 
+	const fetchWorkshop = async () => {
+		const resp = await getWorkshopManagement({ id: params.wspId });
+		if (resp.code == 200) {
+			dispatch(setCurWorkshop(resp.data));
+		} else {
+			message.error(resp.msg);
+		}
+	};
 	const [datasource, setDatasource] = useState([]);
 	const curPage = useRef({
 		pageNum: 1,
@@ -83,7 +130,14 @@ const Incoming: React.FC<IncomingProps> = () => {
 					</ConfigProvider>
 				</div>
 				<div className="flex items-center justify-between mt-4">
-					<div>{StatusView()}</div>
+					<div>
+						<StatusView
+							id={workshop.children?.[0]?.id}
+							status={workshop.children?.[0]?.status}
+							fecthWorkshop={fetchWorkshop}
+							relatedProjectId={workshop.relationProject}
+						/>
+					</div>
 				</div>
 			</div>
 			<div className="mt-16">
